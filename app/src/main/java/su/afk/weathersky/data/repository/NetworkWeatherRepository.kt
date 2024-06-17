6dc0a070f93edfa7ef7c024b215a02896c35969a
Sandy <etozhesandy@gmail.com>
2024-06-17 21:55:47 +0300

    Подправил мелкие недочеты

--- app/src/main/java/su/afk/weathersky/data/repository/NetworkWeatherRepository.kt
+++ app/src/main/java/su/afk/weathersky/data/repository/NetworkWeatherRepository.kt
@@ -5,18 +5,17 @@ import com.plcoding.weatherapp.domain.util.Resource
 import su.afk.weathersky.data.mappers.toNextDaysMapper
 import su.afk.weathersky.data.mappers.toWeatherInfo
 import su.afk.weathersky.data.remote.WeatherApi
-import su.afk.weathersky.data.remote.models.nextDays.NextDaysResponse
 import su.afk.weathersky.domain.repository.WeatherRepository
 import su.afk.weathersky.domain.weather.NextDays
 import su.afk.weathersky.domain.weather.WeatherInfo
 import javax.inject.Inject
 
-class WeatherRepositoryImpl @Inject constructor(
+class NetworkWeatherRepository @Inject constructor(
     private val api: WeatherApi
 ) : WeatherRepository {
 
     override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
-        Log.d("getWeatherData", "getWeatherData: $lat $long")
+        Log.d("NetworkWeatherRepository", "getWeatherData: $lat $long")
         return try {
             Resource.Success(
                 data = api.getWeather(
@@ -31,7 +30,7 @@ class WeatherRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getNextDaysData(lat: Double, long: Double): Resource<NextDays> {
-        Log.d("getNextDaysData", "getWeatherData: $lat $long")
+        Log.d("NetworkWeatherRepository", "getNextDaysData: $lat $long")
         return try {
             val nextDaysData = api.getNextDays(latitude = lat, longitude = long).toNextDaysMapper()
             Resource.Success(data = nextDaysData)
@@ -42,6 +41,6 @@ class WeatherRepositoryImpl @Inject constructor(
     }
 
     companion object {
-        const val ERROR_TEXT = "Произошло неизвественая ошибка"
+        const val ERROR_TEXT = "Произошла неизвественая ошибка"
     }
 }
--- app/src/main/java/su/afk/weathersky/di/RepositoryModule.kt
+++ app/src/main/java/su/afk/weathersky/di/RepositoryModule.kt
@@ -4,9 +4,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import su.afk.weathersky.data.location.DefaultLocationTracker
-import su.afk.weathersky.data.repository.WeatherRepositoryImpl
-import su.afk.weathersky.domain.location.LocationTracker
+import su.afk.weathersky.data.repository.NetworkWeatherRepository
 import su.afk.weathersky.domain.repository.WeatherRepository
 import javax.inject.Singleton
 
@@ -17,6 +15,6 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindWeatherRepository(
-        weatherRepositoryImpl: WeatherRepositoryImpl
+        weatherRepositoryImpl: NetworkWeatherRepository
     ): WeatherRepository
 }
